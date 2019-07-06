3a5195ef7a16d4ef9642fc9f3996ed54822c700a
bwnyasse <risbonyasse@gmail.com>
2019-07-07 00:27:17 +0200

    step1: refactoring

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:learn_flutter/main.dart';
+import 'package:learn_flutter/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
