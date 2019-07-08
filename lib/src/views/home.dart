366304f63a1c7fc7016f36202a6b93f98fe6d68d
bwnyasse <risbonyasse@gmail.com>
2019-07-08 21:30:24 +0200

    step4: Display mock movies

--- lib/src/views/home.dart
+++ lib/src/views/home.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/src/mock/movies.dart';
+import 'package:learn_flutter/src/models/movie.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -20,32 +13,43 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-
-  final List<String> entries = <String>['A', 'B', 'C'];
   final List<int> colorCodes = <int>[600, 500, 100];
+  List<Movie> movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Chargement du mock de movies
+    _loadMovies();
+  }
+
+  _loadMovies() {
+    movies = getMockMovies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: ListView.separated(
         padding: const EdgeInsets.all(8.0),
-        itemCount: entries.length,
+        itemCount: movies.length,
         itemBuilder: (BuildContext context, int index) {
+          Movie movie = movies[index];
+          String originalTitle = movie.originalTitle;
+          String releaseDate = movie.releaseDate;
+
           return Container(
             height: 50,
             color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
+            child: Center(
+              child: Text(
+                "$originalTitle - Date : $releaseDate",
+                style: TextStyle(height: 3.0),
+              ),
+            ),
           );
         },
         separatorBuilder: (BuildContext context, int index) => const Divider(),
